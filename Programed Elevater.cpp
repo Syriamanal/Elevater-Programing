1e77bd6d434a5f443022b5f7bfcb7af637776025
Syriamanal <Shadinachat123@gmail.com>
2013-12-25 16:22:19 -0800

    Create Programed Elevater.cpp

---
+++ Programed Elevater.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+int FloorLeaval;
+
+cout << "Pleas enter a floor number: 1 - 5 \n";
+cin >> FloorLeaval;
+
+switch(FloorLeaval)
+    {
+        case 1:
+        cout << "You enterd " << FloorLeaval;
+        break;
+        case 2:
+        cout << "You enterd " << FloorLeaval;
+        break;
+        case 3:
+        cout << "You enterd " << FloorLeaval;
+        break;
+        case 4:
+        cout << "You enterd " << FloorLeaval;
+        break;
+        case 5:
+        cout << "You enterd " << FloorLeaval;
+        break;
+        default:
+        cout << "Invalid input!";
+    }
+return 0;
+}
