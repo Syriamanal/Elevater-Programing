5256458e70028fc984061a62f8a6d9cb2ec18bb6
Syriamanal <Shadinachat123@gmail.com>
2013-12-25 18:37:02 -0800

    Added more stuff

--- Programed Elevater.cpp
+++ Programed Elevater.cpp
@@ -15,18 +15,23 @@ switch(FloorLeaval)
     {
         case 1:
         cout << "You enterd " << FloorLeaval;
+        cout << "Closeing all doors...";
         break;
         case 2:
         cout << "You enterd " << FloorLeaval;
+        cout << "Closeing all doors...";
         break;
         case 3:
         cout << "You enterd " << FloorLeaval;
+        cout << "Closeing all doors...";
         break;
         case 4:
         cout << "You enterd " << FloorLeaval;
+        cout << "Closeing all doors...";
         break;
         case 5:
         cout << "You enterd " << FloorLeaval;
+        cout << "Closeing all doors...";
         break;
         default:
         cout << "Invalid input!";
